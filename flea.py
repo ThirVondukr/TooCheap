61c4131c5827aee9aebec09e7af62114b14018da
Doctor <thirvondukr@gmail.com>
2021-04-01 00:38:48 +0300

    Added script to get flea prices from tarkov-market api

---
+++ flea.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+import dotenv
+import requests
+
+dotenv.load_dotenv()
+
+response = requests.get(
+    "https://tarkov-market.com/api/v1/items/all",
+    headers={"x-api-key": os.environ["TARKOV_MARKET_API_KEY"]},
+)
+items = response.json()
+items = {item["bsgId"]: price for item in items if (price := item["avg24hPrice"])}
+
+with open("src/flea.json", "w") as file:
+    json.dump(items, file, indent=4, sort_keys=True)
